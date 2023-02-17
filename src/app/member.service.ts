9b581a338fc4f62ecee246233fc1d4ef6cf45864
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2023-02-17 16:50:02 +0900

    メッセージログを追加

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from 'src/app/member';
+import { MessageService } from 'src/app/message.service';
 import { MEMBERS } from 'src/mock-members';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getMember(): Observable<Member[]> {
-    // 実際にはバックエンドから拾ったデータを返す
+    this.messageService.add('MemberService: 社員一覧データを取得しました');
     return of(MEMBERS);
   }
 }
